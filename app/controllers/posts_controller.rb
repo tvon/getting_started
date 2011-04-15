a0f93c98b42b36314c61ed5d56a04c7c208de8ca
Tom von Schwerdtner <tom@smartlogicsolutions.com>
2011-04-15 14:15:57 -0400

    partway through the decent_exposure asciicast, with slight tweaks.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,21 +1,28 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
-
-    respond_to do |format|
-      format.html 
-      format.json { render :json => @posts }
-    end
-
+    respond posts
   end
 
   def show
-    @post = Post.find(params[:id])
+    respond post
+  end
 
+  private
+  def respond(resource)
     respond_to do |format|
-      format.html
-      format.json { render :json => @post }
+      format.html 
+      format.json { render :json => resource }
     end
   end
+
+  def posts
+    @posts ||= Post.order(:created_at)
+  end
+  helper_method :posts
+
+  def post
+    @post ||= params[:id] ? Post.find(params[:id]) : Post.new(params[:post])
+  end
+  helper_method :post
 
 end
