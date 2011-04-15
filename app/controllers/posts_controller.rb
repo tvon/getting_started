1b792c5a79cf81a46ca225c104e26874ddf46e64
Tom von Schwerdtner <tom@smartlogicsolutions.com>
2011-04-15 14:18:45 -0400

    decent_exposure applied to posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,28 +1,12 @@
 class PostsController < ApplicationController
+
+  expose(:post)
+  expose(:posts) { Post.order(:created_at) }
+
   def index
-    respond posts
   end
 
   def show
-    respond post
   end
-
-  private
-  def respond(resource)
-    respond_to do |format|
-      format.html 
-      format.json { render :json => resource }
-    end
-  end
-
-  def posts
-    @posts ||= Post.order(:created_at)
-  end
-  helper_method :posts
-
-  def post
-    @post ||= params[:id] ? Post.find(params[:id]) : Post.new(params[:post])
-  end
-  helper_method :post
 
 end
