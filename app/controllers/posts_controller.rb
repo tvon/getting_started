713122b0c90e5bf250dfdb28c0a9e42596b1ae6f
Tom von Schwerdtner <tom@smartlogicsolutions.com>
2011-04-15 14:52:29 -0400

    post CRUD/scaffold bits added

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,4 +9,31 @@ class PostsController < ApplicationController
   def show
   end
 
+  def new
+  end
+
+  def create
+    if post.save
+      redirect_to posts_path, :notice => "Post created"
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if post.update_attributes(params[:posts])
+      redirect_to posts_path, :notice => "Post updated"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    post.destroy
+    redirect_to posts_path, :notice => "Post deleted"
+  end
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,7 @@
 class Post < ActiveRecord::Base
+  validates :title, :presence => true
+  validates :content, :presence => true,
+    :length => {:minimum => 5 }
 
+  has_many :comments
 end
