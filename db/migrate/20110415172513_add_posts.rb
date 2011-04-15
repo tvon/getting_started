a0bcb4dbd3dfa20b6ab290f7c8fc01219f5a41d8
Tom von Schwerdtner <tom@smartlogicsolutions.com>
2011-04-15 13:34:06 -0400

    add posts...

---
+++ db/migrate/20110415172513_add_posts.rb
@@ -0,0 +1,12 @@
+class AddPosts < ActiveRecord::Migration
+  def self.up
+    create_table :posts do |t|
+      t.string :title
+      t.text :content
+    end
+  end
+
+  def self.down
+    drop_table :posts
+  end
+end
