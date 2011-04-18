0550f188277e134ea706ace14b4a183609bd3018
Tom von Schwerdtner <tom@smartlogicsolutions.com>
2011-04-18 15:12:30 -0400

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110418154130) do
+ActiveRecord::Schema.define(:version => 20110418173934) do
+
+  create_table "assets", :force => true do |t|
+    t.string   "name"
+    t.string   "attachment"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "posts", :force => true do |t|
     t.string   "title"
