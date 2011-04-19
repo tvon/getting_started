d798847d9cb81767bfc2a6c5c525af176ce2284f
Tom von Schwerdtner <tom@smartlogicsolutions.com>
2011-04-19 15:44:18 -0400

    remote adding makes more sense, scrolls to show new comment and highlights the new comment (via css class, not via jquery/effects).  I did however do this before creating the tests....

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,9 @@ class Comment < ActiveRecord::Base
   belongs_to :asset
   belongs_to :user 
 
+  def to_slug
+    # XXX: Not great given the possability for duplication.. shoudl use username+unix timestamp
+    created_at.to_s.downcase.gsub(/[^a-z0-9]/i, '-').gsub(/-{2,}/, '-')
+  end
+
 end
