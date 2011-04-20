32fdefaae1b4ce00a6311cdd480b0fd6c7a3039e
Tom von Schwerdtner <tom@smartlogicsolutions.com>
2011-04-20 13:32:43 -0400

    uplaoder working properly  with rquire line...

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -1,10 +1,12 @@
+require 'carrierwave/orm/mongoid' 
+
 class Asset
   include Mongoid::Document
   include Mongoid::Timestamps
   field :name
   field :attachment
 
-  #mount_uploader :attachment, AttachmentUploader
+  mount_uploader :attachment, AttachmentUploader
 
   has_many :comments
 end
